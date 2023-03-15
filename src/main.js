112b762adccf7b6af2ffc7b31032bc7b9b4af430
Vanessa <vanasusu@gmail.com>
2023-03-15 22:38:36 +0530

    login signup ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './routing/index.js'
+import './assets/loginsignup.css'
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
